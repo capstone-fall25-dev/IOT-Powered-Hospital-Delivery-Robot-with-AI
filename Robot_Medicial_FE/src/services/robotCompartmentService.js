8a921578e0274fe54d10f905b0447a8d22883f73
QuangVu-Dev2003 <vunuong003@gmail.com>
2025-11-13 20:18:08 +0700

    feat: improve compartment selection in AddTask UI

--- Robot_Medicial_FE/src/services/robotCompartmentService.js
+++ Robot_Medicial_FE/src/services/robotCompartmentService.js
@@ -2,17 +2,18 @@
 import axios from "axios";
 import { API_CONFIG } from "@/utils/apiConfig";
 
-// export async function getCompartmentsByRobot(categoryId, robotId) {
-//     const res = await axios.get(
-//         `${API_CONFIG.API_BASE}/RobotCompartments/category/${categoryId}/robot/${robotId}`
-//     );
-//     return res.data;
-// }
-
 // 🔥 Lấy toàn bộ ngăn chứa theo robot
 export async function getCompartmentsByRobot(robotId) {
     const res = await axios.get(
         `${API_CONFIG.API_BASE}/RobotCompartments/robot/${robotId}`
+    );
+    return res.data;
+}
+
+// 🔥 Lấy danh sách Category ngăn chứa
+export async function getAllCategories() {
+    const res = await axios.get(
+        `${API_CONFIG.API_BASE}/CompartmentCategories`
     );
     return res.data;
 }
