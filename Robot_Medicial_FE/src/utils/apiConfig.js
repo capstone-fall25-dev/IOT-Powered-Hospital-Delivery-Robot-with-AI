f9de703d36cf412b04fce6ddb3c498f038e79403
duong20030902 <duonglthe170176@fpt.edu.vn>
2025-11-16 22:17:47 +0700

    updateApiCofig

--- Robot_Medicial_FE/src/utils/apiConfig.js
+++ Robot_Medicial_FE/src/utils/apiConfig.js
@@ -1,7 +1,9 @@
 export const API_CONFIG = {
     //API_BASE: "http://157.66.26.217:5000/api",
-    API_BASE: "http://localhost:5170/api",
-    API_BASE1: "http://localhost:5170",
+    API_BASE: "http://160.187.229.40:5000/api",
+    // API_BASE1: "http://localhost:5170",
+     API_BASE1: "http://160.187.229.40:5000",
+};
 };
 
 export async function apiFetch(url, options = {}) {
