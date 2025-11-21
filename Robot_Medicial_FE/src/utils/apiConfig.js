ba31633eaaa86e232b6a2bfa9d654a63a58076f0
duong20030902 <duonglthe170176@fpt.edu.vn>
2025-11-21 21:49:10 +0700

    updatecofigHTTPS

--- Robot_Medicial_FE/src/utils/apiConfig.js
+++ Robot_Medicial_FE/src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 // src/utils/apiConfig.js
 export const API_CONFIG = {
-    API_BASE: "http://160.187.229.40:5000/api",
-    // API_BASE1: "http://localhost:5170",
-     API_BASE1: "http://160.187.229.40:5000",
+    API_BASE: "https://medigorobot.online/api",
+   // API_BASE1: "http://localhost:5170",
+    API_BASE1: "https://medigorobot.online",
 };
